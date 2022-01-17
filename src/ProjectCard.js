5e6beafcc7e75c39bb77ffc4179bb315bfdfae2b
Mugdhesh Pandkar <mugpand@gmail.com>
2022-01-16 22:38:29 -0500

    fixed even more warnings

--- src/ProjectCard.js
+++ src/ProjectCard.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
@@ -20,7 +20,7 @@ export default function ProjectCard(props) {
                 <CardContent sx={{fontFamily:'Menlo', color:'silver', fontSize: 'calc(10px + 3vmin)'}}>{props.title}</CardContent>
                 <div className='rowC'>
                     <CardContent sx={{width: '60%', fontFamily:'Menlo', color:'#ffe100', marginTop: '-2vh', marginRight: '7.5%', fontSize: 'calc(10px + 2vmin)'}}>{props.description}</CardContent>
-                    <img src={props.image} width='20%' height='20%' onerror='null'/>
+                    <img src={props.image} alt={props.title} width='20%' height='20%' onerror='null'/>
                 </div>
                 <CardContent sx={{width: '60%', fontFamily:'Menlo', color:'gray', marginTop: '-3vh', marginRight: '5%', fontSize: 'calc(10px + 2vmin)'}}>Technologies: <div className='tech' sx={{colo:'blue'}}>{props.technologies}</div></CardContent>
                 <CardContent sx={{width: '60%', fontFamily:'Menlo', color:'gray', marginTop: '-3vh', marginRight: '5%', fontSize: 'calc(10px + 2vmin)'}}>Repository: <a href={props.link}>Click here!</a></CardContent>
